4aaa75d75810fbd972cc8423d0ee04fbd8b022c3
Albert Tian <atian@berkeley.edu>
2023-07-23 14:04:42 -0700

    content: multipage app, analysis page

--- captions.py
+++ captions.py
@@ -2,41 +2,46 @@ import streamlit as st
 import pandas as pd
 
 # defaults
-TEMP_CSV = 'temp.csv'
+TEMP_CSV = "temp.csv"
 
 
 # For List[Weight] and List[List[Reps]], generate a line
-# s["Reps list"] = [[a, b], [c, d]] 
+# s["Reps list"] = [[a, b], [c, d]]
 # s["Weight"] = [x, y]
 def generateCaptionLine(s):
-    name, repsList, weights, unit = s["Exercise Name"], s["Reps"], s["Weight"], s["Unit"]
+    name, repsList, weights, unit = (
+        s["Exercise Name"],
+        s["Reps"],
+        s["Weight"],
+        s["Unit"],
+    )
     if name == "Intensity":
         return f"Intensity: {repsList[0][0]}"
 
     blocks = []
     for weight, reps in zip(weights, repsList):
         # Round to the nearest .5 (for floating point errors) and eliminate trailing .0
-        weightStr = str(round(weight * 2) / 2).replace('.0', '')
+        weightStr = str(round(weight * 2) / 2).replace(".0", "")
         # NOW WE GOT KILO PLATES MF YAAAA
-        weightTag = f', {weightStr}{unit}' if weight else ''
-        block = ''
+        weightTag = f", {weightStr}{unit}" if weight else ""
+        block = ""
         # Amrap rule: 4x4 -> 8 instead of 4/4/4/4/8. Avoids 1x5 -> 6 format; will be 5/6 instead
         if len(reps) > 2 and len(set(reps)) == 2 and len(set(reps[:-1])) == 1:
-            block = f'{len(reps)-1}x{reps[0]} -> {reps[-1]}{weightTag}'
+            block = f"{len(reps)-1}x{reps[0]} -> {reps[-1]}{weightTag}"
 
         # When we have 1 rep count for all sets. 3x12 format
         elif len(set(reps)) == 1:
-            block = f'{len(reps)}x{reps[0]}{weightTag}'
+            block = f"{len(reps)}x{reps[0]}{weightTag}"
 
         # Varrying rep counts through the sets. 8/8/7/5 format
         else:
-            repStr = '/'.join([str(rep) for rep in reps])
-            block = f'{repStr}{weightTag}'
+            repStr = "/".join([str(rep) for rep in reps])
+            block = f"{repStr}{weightTag}"
 
         blocks.append(block)
 
     meat = " | ".join(blocks)
-    return f'{name}: {meat}'
+    return f"{name}: {meat}"
 
 
 # Will change day_week_dict with each iteration
@@ -45,9 +50,13 @@ def generateCaption(s, user_name, program_name, day_week_dict):
     date, exerciseLine = s["Date parsed"], s["Exercise line"]
     header = f"{date}: {user_name}"
     if program_name:
-        header +=  f" {program_name}"
+        header += f" {program_name}"
     if day_week_dict and day_week_dict["week"] > 0:
-        week, day, days_per_week = day_week_dict["week"], day_week_dict["day"], day_week_dict["days_per_week"]
+        week, day, days_per_week = (
+            day_week_dict["week"],
+            day_week_dict["day"],
+            day_week_dict["days_per_week"],
+        )
         header += f" week {week} day {day}"
         # Decrement week and day
         day_week_dict["week"] -= 1 if day == 1 else 0
@@ -57,20 +66,20 @@ def generateCaption(s, user_name, program_name, day_week_dict):
 
 def displayCaption(s):
     st.write(
-        f'''
+        f"""
         ```
         {s}
-        '''
+        """
     )
 
 
 def main():
-    st.write("### Either upload or paste strong data brehhh cnme o n")
+    st.write("### Either upload or paste strong data!")
     # Input csv
     csv_file = st.file_uploader("Upload a strong export", type=["csv"])
     text_input = st.text_area("Paste strong export")
     if text_input:
-        with open(TEMP_CSV, 'w') as out:
+        with open(TEMP_CSV, "w") as out:
             out.write(text_input)
     if not (csv_file or text_input):
         return
@@ -99,46 +108,61 @@ def main():
     if not user_name:
         return
 
-    with st.spinner("Loading..."): 
+    with st.spinner("Loading..."):
         # Start parsing
         csv_to_open = csv_file if csv_file else TEMP_CSV
         df = pd.read_csv(csv_to_open, na_filter=False)
         df["Date"] = pd.to_datetime(df["Date"])
 
         # Group by Date, Exercise and Weight. Can agg reps using .apply. sort=False to preserve exercise order
-        df = df.groupby(["Date", "Exercise Name", "Weight"], sort=False)["Reps"].agg(list).reset_index()
-        df = df.groupby(["Date", "Exercise Name"],sort=False).agg(list).reset_index()
+        df = (
+            df.groupby(["Date", "Exercise Name", "Weight"], sort=False)["Reps"]
+            .agg(list)
+            .reset_index()
+        )
+        df = df.groupby(["Date", "Exercise Name"], sort=False).agg(list).reset_index()
 
         # Exercise name cleanup
-        # TODO: Add nicknames for exercises 
+        # TODO: Add nicknames for exercises
         df["Exercise Name"] = df["Exercise Name"].str.replace(" (Barbell)", "")
         df["Exercise Name"] = df["Exercise Name"].str.replace(" (Dumbbell)", "")
         df["Exercise Name"] = df["Exercise Name"].str.replace(" (Machine)", "")
 
-        # Cut off df to only number of captions 
-        num_caps = st.slider("How many captions", min_value=0,max_value=40, value=6)
-        unique_dates = df['Date'].unique()[::-1]
+        # Cut off df to only number of captions
+        num_caps = st.slider("How many captions", min_value=0, max_value=40, value=6)
+        unique_dates = df["Date"].unique()[::-1]
         last_workouts_dates = unique_dates[:num_caps]
-        num_rows = df[df['Date'].isin(last_workouts_dates)].shape[0]
+        num_rows = df[df["Date"].isin(last_workouts_dates)].shape[0]
         df = df.tail(num_rows)
 
         # Sort by date (creates temporary Order column)
-        df['Order'] = df.groupby('Date').cumcount()
-        df = df.sort_values(by=['Date', 'Order'], ascending=[False, True])
-        df = df.drop(columns='Order')
+        df["Order"] = df.groupby("Date").cumcount()
+        df = df.sort_values(by=["Date", "Order"], ascending=[False, True])
+        df = df.drop(columns="Order")
 
-        # Edit units, default lbs 
-        df['Unit'] = 'lbs'
+        # Edit units, default lbs
+        df["Unit"] = "lbs"
         st.write("### Edit units if necessary")
         df = st.data_editor(df)
 
-        # Generate per EXERCISE caption lines 
+        # Generate per EXERCISE caption lines
         df["Exercise line"] = df.apply(generateCaptionLine, axis=1)
 
         # Generate per DATE caption lines (joins with newlines)
-        df = df.groupby(["Date"])["Exercise line"].apply(lambda l: '\n'.join(l)).reset_index()
-        df["Date parsed"] = df["Date"].dt.strftime("-%m/-%d").str.replace("-0", "-").str.replace("-", "")
-        df["Caption"] = df.apply(lambda s: generateCaption(s, user_name, program_name, day_week_dict), axis=1)
+        df = (
+            df.groupby(["Date"])["Exercise line"]
+            .apply(lambda l: "\n".join(l))
+            .reset_index()
+        )
+        df["Date parsed"] = (
+            df["Date"]
+            .dt.strftime("-%m/-%d")
+            .str.replace("-0", "-")
+            .str.replace("-", "")
+        )
+        df["Caption"] = df.apply(
+            lambda s: generateCaption(s, user_name, program_name, day_week_dict), axis=1
+        )
         df = df.sort_values("Date", ascending=False)
 
         # Display to output
---
+++ pages/analysis.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+def main():
+    st.write("Hello analysis@!!!")
+
+
+main()
